118a6eef6bf8e5a59399988211d01c292aa4313a
Titor07 <dumange.victor@gmail.com>
2022-10-23 21:55:13 +0200

    first version of pp_prg

--- testcompilo.py
+++ testcompilo.py
@@ -10,6 +10,20 @@ exp : SIGNED_NUMBER                     -> exp_nombre
 | exp OPBIN exp                         -> exp_opbin
 | "(" exp ")"                           -> exp_par
 
+bcom : (com)*
+
+com : lhs "=" exp ";"                   -> assignation
+| "if" "(" exp ")" "{" bcom "}"         -> if
+| "while" "(" exp ")" "{" bcom "}"      -> while
+| "print" "(" exp ")"                   -> print
+
+prg : "main" "(" var_list ")" "{" bcom "return" "(" exp ")" ";" "}" 
+
+var_list :                              -> vide
+| IDENTIFIER ("," IDENTIFIER)*          -> aumoinsune
+
+lhs: TABLE"[" exp "]"                   -> ele_tab
+| IDENTIFIER                            -> var
 
 IDENTIFIER : TABLE | INTEGER
 
@@ -23,7 +37,7 @@ OPBIN : /[+*\->]/
 %import common.SIGNED_NUMBER
 %ignore WS
 """,
-start = "exp")
+start = "prg")
 
 def pp_exp(e) :
     if e.data in {"exp_nombre", "exp_var_int", "exp_var_tab"} :
@@ -41,13 +55,37 @@ def pp_exp(e) :
     else:#opbinaire
         return f"{pp_exp(e.children[0])} {e.children[1].value} {pp_exp(e.children[2])}"
 
+def pp_com(c) :
+    if c.data == "assignation":
+        return f"{pp_lhs(c.children[0])} = {pp_exp(c.children[1])};"
+    elif c.data == "if":
+        return f"if ({pp_exp(c.children[0])}) {{ {pp_bcom(c.children[1])} }}"
+    elif c.data == "while":
+        return f"while ({pp_exp(c.children[0])}) {{ {pp_bcom(c.children[1])}}}"
+    elif c.data == "print":
+        return f"print({pp_exp(c.children[0])})"
+
+def pp_bcom(bc) :
+    return "\n"  + "\n".join([pp_com(c) for c in bc.children]) +"\n"
+
+def pp_lhs(l) :
+    if l.data == "ele_tab":
+        return f"{l.children[0].value}[{pp_exp(l.children[1])}]"
+    else :
+        return l.children[0].value
+
+def pp_prg(p) :
+    L = pp_var_list(p.children[0])
+    C = pp_bcom(p.children[1])
+    R = pp_exp(p.children[2])
+    return "main (%s) {%s return (%s);\n}" % (L,C,R)
+
+def pp_var_list(vl) :
+    return ", ".join([t.value for t in vl.children])
+
 #examples
-print(pp_exp(grammaire.parse("tx")))
-print(pp_exp(grammaire.parse("ix")))
-print(pp_exp(grammaire.parse("3")))
-print(pp_exp(grammaire.parse("len(tx)")))
-print(pp_exp(grammaire.parse("concat(tx, ty)")))
-print(pp_exp(grammaire.parse("int[ix+3]")))
+print(pp_prg(grammaire.parse("main(iX,tY){ix = tY; return (ix);}")))
+
 
 
 
